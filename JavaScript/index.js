df4b8900574ce9c919dc96271dba21c2ce470b53
AhmadMaree <anahek.smy.91@gmail.com>
2020-09-14 12:10:41 +0300

    add form tag

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -10,7 +10,7 @@ var user_name = document.getElementsByClassName("login-username");
     btn[0].addEventListener("click" , function () {
 
         localStorage.setItem("username", user_name[0].value);
-        window.document.location= '../Main.html' ;
+       //  window.document.location= '../Main.html' ;
        // window.document.location= '../Main.html'+ '?username='+user_name.value;
     
     });
